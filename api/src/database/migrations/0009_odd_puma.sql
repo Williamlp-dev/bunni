173bf6cf3d51fb4d6cd56c47a28e91d82a59e861
Williamlp-dev <williamlp.dev@gmail.com>
2026-03-25 19:41:56 -0300

    fix(api): add if exists to drop table statements in migration

--- api/src/database/migrations/0009_odd_puma.sql
+++ api/src/database/migrations/0009_odd_puma.sql
@@ -1,11 +1,11 @@
-DROP TABLE "accounts" CASCADE;--> statement-breakpoint
-DROP TABLE "blocks" CASCADE;--> statement-breakpoint
-DROP TABLE "conversation_participants" CASCADE;--> statement-breakpoint
-DROP TABLE "conversations" CASCADE;--> statement-breakpoint
-DROP TABLE "friend_requests" CASCADE;--> statement-breakpoint
-DROP TABLE "friendships" CASCADE;--> statement-breakpoint
-DROP TABLE "message_deletions" CASCADE;--> statement-breakpoint
-DROP TABLE "messages" CASCADE;--> statement-breakpoint
-DROP TABLE "sessions" CASCADE;--> statement-breakpoint
-DROP TABLE "users" CASCADE;--> statement-breakpoint
-DROP TABLE "verifications" CASCADE;
+DROP TABLE IF EXISTS "accounts" CASCADE;--> statement-breakpoint
+DROP TABLE IF EXISTS "blocks" CASCADE;--> statement-breakpoint
+DROP TABLE IF EXISTS "conversation_participants" CASCADE;--> statement-breakpoint
+DROP TABLE IF EXISTS "conversations" CASCADE;--> statement-breakpoint
+DROP TABLE IF EXISTS "friend_requests" CASCADE;--> statement-breakpoint
+DROP TABLE IF EXISTS "friendships" CASCADE;--> statement-breakpoint
+DROP TABLE IF EXISTS "message_deletions" CASCADE;--> statement-breakpoint
+DROP TABLE IF EXISTS "messages" CASCADE;--> statement-breakpoint
+DROP TABLE IF EXISTS "sessions" CASCADE;--> statement-breakpoint
+DROP TABLE IF EXISTS "users" CASCADE;--> statement-breakpoint
+DROP TABLE IF EXISTS "verifications" CASCADE;
